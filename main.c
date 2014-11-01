400b2938782725c20d831a6e81789189ba3d4618
M. J. Wharton <mike.wharton@gmail.com>
2014-11-01 16:26:43 +0000

    modified:   main.c

--- main.c
+++ main.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <fcntl.h> // open
 #include <unistd.h> // close
+#include <string.h> // strstr
 
 #define Dbg(...) \
 { \
@@ -73,7 +74,7 @@ check_dir(char *dirpath)
 }
 
 int 
-colours()
+test_colours()
 {
 	int		colour_count = 15;
 	char           *colours[] =
@@ -122,16 +123,125 @@ colours()
 	return 0;
 }
 
+int curl_grab(char *url, char *buffer_file)
+{
+
+}
+
+/* From example at http://curl.haxx.se/libcurl/c/simplessl.html */
+
+int curl_grab_ssl(char *url, char *buffer_file)
+{
+  int i;
+  CURL *curl;
+  CURLcode res;
+  FILE *headerfile;
+  const char *pPassphrase = NULL;
+ 
+  static const char *pCertFile = "testcert.pem";
+  static const char *pCACertFile="cacert.pem";
+ 
+  const char *pKeyName;
+  const char *pKeyType;
+ 
+  const char *pEngine;
+ 
+#ifdef USE_ENGINE
+  pKeyName  = "rsa_test";
+  pKeyType  = "ENG";
+  pEngine   = "chil";            /* for nChiper HSM... */ 
+#else
+  pKeyName  = "testkey.pem";
+  pKeyType  = "PEM";
+  pEngine   = NULL;
+#endif
+ 
+  headerfile = fopen("dumpit", "w");
+ 
+  curl_global_init(CURL_GLOBAL_DEFAULT);
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    /* what call to write: */ 
+    curl_easy_setopt(curl, CURLOPT_URL, "https://api.pinboard.in/v1/posts/all");
+    curl_easy_setopt(curl, CURLOPT_HEADERDATA, headerfile);
+ 
+    for(i = 0; i < 1; i++) /* single-iteration loop, just to break out from */ 
+    {
+      if (pEngine)             /* use crypto engine */ 
+      {
+        if (curl_easy_setopt(curl, CURLOPT_SSLENGINE,pEngine) != CURLE_OK)
+        {                     /* load the crypto engine */ 
+          fprintf(stderr,"can't set crypto engine\n");
+          break;
+        }
+        if (curl_easy_setopt(curl, CURLOPT_SSLENGINE_DEFAULT,1L) != CURLE_OK)
+        { /* set the crypto engine as default */ 
+          /* only needed for the first time you load
+ *              a engine in a curl object... */ 
+          fprintf(stderr,"can't set crypto engine as default\n");
+          break;
+        }
+      }
+      /* cert is stored PEM coded in file... */ 
+      /* since PEM is default, we needn't set it for PEM */ 
+      curl_easy_setopt(curl,CURLOPT_SSLCERTTYPE,"PEM");
+ 
+      /* set the cert for client authentication */ 
+      curl_easy_setopt(curl,CURLOPT_SSLCERT,pCertFile);
+ 
+      /* sorry, for engine we must set the passphrase
+ *          (if the key has one...) */ 
+      if (pPassphrase)
+        curl_easy_setopt(curl,CURLOPT_KEYPASSWD,pPassphrase);
+ 
+      /* if we use a key stored in a crypto engine,
+ *          we must set the key type to "ENG" */ 
+      curl_easy_setopt(curl,CURLOPT_SSLKEYTYPE,pKeyType);
+ 
+      /* set the private key (file or ID in engine) */ 
+      curl_easy_setopt(curl,CURLOPT_SSLKEY,pKeyName);
+ 
+      /* set the file with the certs vaildating the server */ 
+      curl_easy_setopt(curl,CURLOPT_CAINFO,pCACertFile);
+ 
+      /* disconnect if we can't validate server's cert */ 
+      curl_easy_setopt(curl,CURLOPT_SSL_VERIFYPEER,1L);
+ 
+      /* Perform the request, res will get the return code */ 
+      res = curl_easy_perform(curl);
+      /* Check for errors */ 
+      if(res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+ 
+      /* we are done... */ 
+    }
+    /* always cleanup */ 
+    curl_easy_cleanup(curl);
+  }
+ 
+  curl_global_cleanup();
+ 
+  return 0;
+}
+
 int
 main(int argc, char *argv[])
 {
 	char           *filepath;
+	char colour_arg[] = "-c";
 
 	puts("Bonza!");
 	error_mode = 's';	/* Makes Stopif use abort() */
 
-	colours();
-	return 0;
+	/* TODO: replace with getopt */
+	for (int j = 0; j < argc; j++)
+		if (strstr(argv[j], colour_arg) != NULL)
+		{
+			test_colours();
+			return 0;
+		}
 
 	/*
 	 * Steps > See if JSON file exists and is not old > If not, download
